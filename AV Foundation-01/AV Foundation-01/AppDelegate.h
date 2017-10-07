e115af3e670978dce045a4e43dfcf373f6ecf2ce
XBDZ <18730261828@163.com>
2017-10-07 21:42:28 +0800

    初始化项目

---
+++ AV Foundation-01/AV Foundation-01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AV Foundation-01
+//
+//  Created by Mr Liu on 2017/10/7.
+//  Copyright © 2017年 Mr Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AV Foundation-01/AV Foundation-01Tests/AV_Foundation_01Tests.m
@@ -0,0 +1,39 @@
+//
+//  AV_Foundation_01Tests.m
+//  AV Foundation-01Tests
+//
+//  Created by Mr Liu on 2017/10/7.
+//  Copyright © 2017年 Mr Liu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AV_Foundation_01Tests : XCTestCase
+
+@end
+
+@implementation AV_Foundation_01Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
