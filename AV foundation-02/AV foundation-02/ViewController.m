59e9958693cec4a37fc1f426fd546617e76ecaec
apple <apple@apples-mbp.lan>
2017-10-19 20:53:48 +0800

    第三次提交

--- AV foundation-02/AV foundation-02/ViewController.m
+++ AV foundation-02/AV foundation-02/ViewController.m
@@ -9,10 +9,11 @@
 #import "ViewController.h"
 #import "THPlayerController.h"
 #import <AVFoundation/AVFoundation.h>
-@interface ViewController ()
+@interface ViewController ()<THPlayerControllerDelegate>
 
 //必须强引用
 @property(nonatomic,strong)THPlayerController *playerVC;
+@property (weak, nonatomic) IBOutlet UIButton *playButton;
 
 @end
 
@@ -22,6 +23,7 @@
     [super viewDidLoad];
     
     self.playerVC = [[THPlayerController alloc]init];
+    self.playerVC.delegate = self;
     
     
 }
@@ -30,19 +32,28 @@
 
     if (!self.playerVC.isPlaying) {
         [self.playerVC play];
+        [sender setTitle:@"播放" forState:UIControlStateNormal];
+    }else{
+        [self.playerVC stop];
+        [sender setTitle:@"停止" forState:UIControlStateNormal];
     }
-}
-
-- (IBAction)stopAction:(UIButton *)sender {
-}
-
--(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-{
-   
-    
+    sender.selected = !sender.selected;
 }
 
 
+- (void)playbackStopped {
+    self.playButton.selected = NO;
+     [self.playButton setTitle:@"播放" forState:UIControlStateNormal];
+}
+
+- (void)playbackBegan {
+    self.playButton.selected = YES;
+     [self.playButton setTitle:@"停止" forState:UIControlStateNormal];
+}
+
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
