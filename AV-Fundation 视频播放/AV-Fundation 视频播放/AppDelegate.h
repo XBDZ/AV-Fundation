389b344be688fa33915c77b8e547e97d7c7cf0f0
XBDZ <18730261828@163.com>
2017-11-05 14:24:51 +0800

    初始化 视频播放

---
+++ AV-Fundation 视频播放/AV-Fundation 视频播放/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AV-Fundation 视频播放
+//
+//  Created by apple on 17/11/5.
+//  Copyright © 2017年 Mr Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AV-Fundation 视频播放/AV-Fundation 视频播放/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AV-Fundation 视频播放
+//
+//  Created by apple on 17/11/5.
+//  Copyright © 2017年 Mr Liu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
